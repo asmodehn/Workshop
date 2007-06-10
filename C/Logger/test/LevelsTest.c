14559424add5e2dfc53136de4a392d61bbf7b6d3
alex <alex@66de3724-7412-dc11-b2c4-0019db4d19a6>
2007-06-10 15:25:46 +0000

    improved leveltest

--- C/Logger/test/LevelsTest.c
+++ C/Logger/test/LevelsTest.c
@@ -9,9 +9,27 @@ int main ( int argc, char* argv[] )
 
 	logger_set_target(logfile);
 	
-	if ( logger_log("test\n") == 5 )
-		return 0;
-	else
+	logger_filter_lvl_out(LOGGER_WARNING_LVL);
+	
+	if ( logger_dbglog("test\n") != 0 )
+	{
 		return 1; /* error : number of character outputted different from what was expected */
-	return 0;
+	}
+
+	if ( logger_log("test\n") != 0 )
+	{
+		return 1; /* error : number of character outputted different from what was expected */
+	}
+
+	if ( logger_warning("test\n") != 5 )
+	{
+		return 1; /* error : number of character outputted different from what was expected */
+	}
+
+	if ( logger_error("test\n") != 5 )
+	{
+		return 1; /* error : number of character outputted different from what was expected */
+	}
+
+	return 0 ; /* everything was fine */
 }
