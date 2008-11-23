1375a480bafa67ca9b94ec26588942f3698886dc
alex <alex@66de3724-7412-dc11-b2c4-0019db4d19a6>
2008-11-23 12:08:44 +0000

    now image in panels...

--- Rebol/Hangman.r
+++ Rebol/Hangman.r
@@ -1,34 +1,82 @@
 REBOL [
 	title: "Hangman"
 	author: "Asmodehn"
-	version: 0.0.1
+	version: 0.1.1
 	date: 15-Nov-2008
 	copyright: "Freeware"
 ]
 
-rebomb: context [
+hangman: context [
 	sprite-size: 20x20
+	text-size: 18
+	;TODO :  font loading
+	main-size: 320x240
 	pause: no
-	
+	hangword: "hangman"
+	letter-list: ['a 'b 'c 'd ]
+
 	init: reduce [ random/seed now ]
 
-	; the main layout. originally with startup face. will hcange later based on game state
-	lay: make face [
-			size: 400x400
-			text: "ReBomb"
-			pane: [ startup ]		 
+	faces: context[	
+		start-btn:  make face [
+					image: load %Hangman-data/Start.png
+					effect: [ fit ]
+					color:yellow
+					action: [ view layout [ text "yop" ] ]
+				]
+		hangpic: make face [
+					image: load %Hangman-data/stick0.bmp 
+					effect: [ fit ]
+					color:blue
+				]
+		hangtitle: make face [
+					text: "Hangman"
+					color: red
+				]
+	]
+	
+
+	debug: layout [    
+			    label "Debug:"
+			    console: field 100 [attempt [do console/text]]
 	]
 
-	startup: layout [
-				text "Hangman"
-				button "Start" [ lay/pane: [ gamerunning ] show lay ]
-		]
 
-	gamerunning: layout[ box red 200x200 ]
+	;startup layout
+	startup: make face [
+				offset: 0x0
+				size: main-size
+				color: green
+				pane: reduce [
+					faces/hangpic
+					faces/hangtitle
+					faces/start-btn
+				]
+			]
+			faces/hangtitle/offset: 0x100
+			faces/hangtitle/size: 100x20
+			faces/start-btn/offset: 0x150
+			faces/start-btn/size: 100x30
+			;faces/start-btn/action: 
+
+	gamerunning: layout [
+				text hangword
+				text "letters used"				
+				text "Choose a letter"
+				]
 
 	gameover: make face []
 
 	gamesuccess: make face []
+
+	; the main layout. originally with startup face. will change based on game state
+	lay: make face [
+			offset: 0x0
+			color: black
+			size: main-size
+			text: "Hangman"
+			pane: reduce [ startup ]
+	]
 
 ]
 
@@ -45,9 +93,9 @@ insert-event-func func [face event] bind [
 		return none
 	]
 	event
-] in rebomb 'self
+] in hangman 'self
 
 if any [not system/script/args empty? form system/script/args] [
-	rebomb/init
-    view center-face rebomb/lay
+	hangman/init
+    view center-face hangman/lay
 ]
--- Rebol/index.r
+++ Rebol/index.r
@@ -6,5 +6,6 @@ file "Info" %info.txt
 file "Bomber" %Bomber.r
 file "Hangman" %Hangman.r
 file "Library" %Library.r
+file "Layout Writer" %layout-writer.r
 file "OpenGLDemo" %opengl.r
 file "Space Game" %space-game.r
